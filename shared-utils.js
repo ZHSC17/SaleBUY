e021b23f9a488186747878b5c6645fc405ec234a
ZHSC17 <32410651+ZHSC17@users.noreply.github.com>
2025-09-04 00:30:23 +0800

    Update shared-utils.js

--- shared-utils.js
+++ shared-utils.js
@@ -336,7 +336,7 @@ async function startTradingCycle(times = 10) {
 
 function CreateUI() {
 
-    logPanel("UI创建完成")
+    logToPanel("UI创建完成")
       // UI按钮
     const btn = document.createElement('button');
     btn.textContent = '🚀 开始' + coinName + '自动交易';
